0107b1f2d1106207124d58425b11e4d687b854ee
Denis Demidov <dennis.demidov@gmail.com>
2017-11-02 15:54:07 +0300

    update unio.jl

--- unio.jl
+++ unio.jl
@@ -1,9 +1,22 @@
+using JLD
 using HDF5
 
 function h5_load(fname)
     f = h5open(fname)
     for n in names(f)
-        v = Symbol(n)
-        @eval (($v) = read($f, $n))
+        if n[1] != '_'
+            v = Symbol(n)
+            @eval (($v) = read($f, $n))
+        end
     end
+end
+
+function load_inputs()
+    files = readdir("in")
+
+    for f in readdir("in")
+        h5_load(joinpath("in", f))
+    end
+
+    return files[1]
 end
